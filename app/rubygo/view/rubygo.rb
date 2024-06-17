e006dfddcf588bd5d802b39a05655bfe0181dc6c
Tim Standen <timmstanden@gmail.com>
2024-06-17 11:35:09 -0700

    Add playing tokens on board

--- app/rubygo/view/rubygo.rb
+++ app/rubygo/view/rubygo.rb
@@ -1,14 +1,21 @@
+Cell = Struct.new (:player)
+
 class Rubygo
   module Model
     class Game 
       
-      attr_accessor :height, :width, :scale, :name
+      attr_accessor :height, :width, :scale, :name, :tokens, :cur_player
       
       def initialize(height = 12, width = 12, scale = 70, name = "Go Game")
         @height = height
         @width = width
         @scale = scale
-        @name = name
+        @tokens = height.times.map do
+          width.times.map do
+            Cell.new(0) 
+          end
+        end
+        @cur_player = 1
       end
     end
   end
@@ -26,12 +33,15 @@ class Rubygo
           game.height.times.map do |row|
             horizontal_box {
               padded false
-
               game.width.times.map do |column|
                 half = game.scale / 2
                 area {
                   square(0, 0, game.scale) {
                     fill r: 240, g: 215, b: 141, a: 1.0
+                    on_mouse_up do |clicked_event|
+                      game.tokens[row][column][:player] = game.cur_player
+                      game.cur_player = -game.cur_player
+                    end
                   }
                   line(half,row == 0 ? half : 0, half, row == (game.height - 1)? half : game.scale) {
                     stroke 0x000000
@@ -39,6 +49,14 @@ class Rubygo
                   line(column == 0 ? half : 0, half, column == (game.width - 1) ? half : game.scale, half){
                     stroke 0x000000
                   } 
+                  circle(half, half, half - 8) {
+                    fill <= [game.tokens[row][column], :player, on_read: -> (player) { 
+                      return if player == 0
+                      return :white if player == 1
+                      :black 
+                    } ]
+                    
+                  }
                 }
               end
             }
@@ -54,10 +72,8 @@ class Rubygo
     class NewGameWindow
       include Glimmer::LibUI::CustomWindow
       option :on_create, default: lambda { |user| }
-      
-      before_body do
-        @game = Model::Game.new
-      end
+      option :height, default: 12
+      option :width, default: 12
 
       body {
         window { |new_game_window|
@@ -69,13 +85,13 @@ class Rubygo
                   horizontal_box {
                     label('Board Width')
                     spinbox(2, 20) {
-                      value <=> [@game, :width]
+                      value <=> [self, :width]
                     }
                   }
                   horizontal_box {
                     label('Board Height')
                     spinbox(2, 20) {
-                      value <=> [@game, :height]
+                      value <=> [self, :height]
                     }
                   }
               }
@@ -89,7 +105,7 @@ class Rubygo
               }
               button("New Game") {
                 on_clicked do
-                  on_create.call(@game)
+                  on_create.call(Model::Game.new(height, width))
                   new_game_window.destroy
                 end
               }
@@ -106,15 +122,6 @@ class Rubygo
     class Rubygo
       include Glimmer::LibUI::Application
 
-      ## Add options like the following to configure CustomWindow by outside consumers
-      #
-      # options :title, :background_color
-      # option :width, default: 320
-      # option :height, default: 240
-
-            ## Use before_body block to pre-initialize variables to use in body and
-      #  to setup application menu
-      #
       before_body do
         @game = Model::Game.new
 
@@ -124,11 +131,11 @@ class Rubygo
               on_create = lambda { |game| 
                 @game.height = game.height
                 @game.width = game.width
+                @game.tokens = game.tokens
               }
               new_game_window(on_create: on_create).show
             end
           }
-          menu_item('Edit Current Game')
           menu_item('Load Game')
 
           # Enables quitting with CMD+Q on Mac with Mac Quit menu item
@@ -165,7 +172,7 @@ class Rubygo
             text <= [@game, :name]
           }
           vertical_box {
-            content(@game, :width) {
+            content(@game, :tokens) {
               game_board(game: @game)
             }
           }
